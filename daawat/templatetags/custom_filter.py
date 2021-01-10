cc3495f8aef64fc4a693648c51577bcf34706dcc
Shabaz <53282701+Kingpins@users.noreply.github.com>
2021-01-10 16:07:50 +0530

    Add files via upload

--- daawat/templatetags/custom_filter.py
+++ daawat/templatetags/custom_filter.py
@@ -11,6 +11,14 @@ def currency(number):
 def change_to_date(timestamp):
     return datetime.fromtimestamp(timestamp)
 
+@register.filter(name='no_orders_pending')
+def no_orders_pending(customer_id,orders):
+    count = 0
+    for order in orders:
+        if order["customer_id"] == customer_id:
+            if order["status"] == False:
+                count += 1
+    return count
 
 @register.filter(name='multiply')
 def multiply(number , number1):
