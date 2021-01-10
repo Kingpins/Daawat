9501e34971db60872709e95cb9cec2f908543429
Shabaz <53282701+Kingpins@users.noreply.github.com>
2021-01-10 17:50:15 +0530

    Update speak_service.py

--- daawat/service/speak_service.py
+++ daawat/service/speak_service.py
@@ -9,7 +9,7 @@ def HotelIntro(HotelName,Bio):
     text = "Hello, I'm Pari, Dear Customer, Welcome to " + HotelName +", This hotel is known for "+ Bio +", In order to begin your digital menu experience, Please provide your good name and click Go to menu button, Thank you."
     myobj = gTTS(text=text,lang='en',slow=False,)
     path_of_storage = os.path.join(BASE_DIR,'audioclips')
-    myobj.save(path_of_storage+"\\"+HotelName+".mp3")
+    myobj.save(path_of_storage+"/"+HotelName+".mp3")
     storage_link = storage.child("hotelAudioClips/"+HotelName+".mp3").put(path_of_storage+"/"+HotelName+".mp3")
     
 def HotelCategories(HotelCategories,HotelName):
@@ -19,12 +19,12 @@ def HotelCategories(HotelCategories,HotelName):
     text += "Start placing your orders, Enjoy the Daawat! Thank you."
     myobj = gTTS(text=text,lang='en',slow=False,)
     path_of_storage = os.path.join(BASE_DIR,'audioclips')
-    myobj.save(path_of_storage+"\\"+HotelName+"Categories.mp3")
+    myobj.save(path_of_storage+"/"+HotelName+"Categories.mp3")
     storage_link = storage.child("hotelAudioClips/"+HotelName+"Categories.mp3").put(path_of_storage+"/"+HotelName+"Categories.mp3")
 
 def PlaceOrder():
     text = "You can place as many order as you want, until you make your bill in the My Bill section, Thank You"
     myobj = gTTS(text=text,lang='en',slow=False,)
     path_of_storage = os.path.join(BASE_DIR,'audioclips')
-    myobj.save(path_of_storage+"\\PlaceOrder.mp3")
+    myobj.save(path_of_storage+"/PlaceOrder.mp3")
     storage_link = storage.child("hotelAudioClips/PlaceOrder.mp3").put(path_of_storage+"/PlaceOrder.mp3")
